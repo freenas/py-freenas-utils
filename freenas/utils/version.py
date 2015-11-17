6b7825f10bdd2896138caabe0dacc8aca281f02b
William Grzybowski <wg@FreeBSD.org>
2015-11-17 14:03:19 -0200

    Add a verion module to utils to handle python package version
    This might not be the best place to put it but its good enough for now.

---
+++ freenas/utils/version.py
@@ -0,0 +1,40 @@
+import os
+import re
+import sys
+
+
+def get_version(ver=None):
+    # If a version.txt file exists, use that as version
+    verfile = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), 'version.txt')
+    if os.path.exists(verfile):
+        with open(verfile, 'r') as f:
+            return f.read().strip('\n').strip('\r')
+
+    if ver is None:
+        ver = os.environ.get('VERSION')
+    if ver:
+        version = ver.split('-', 1)[0]
+        prerelease = None
+        reg = re.search(r'(ALPHA|BETA|RC)(\d+)?', ver, re.I)
+        if reg:
+            stage = reg.group(1).lower()
+            if stage == 'alpha':
+                prerelease = 'a'
+            elif stage == 'beta':
+                prerelease = 'b'
+            elif stage == 'rc':
+                prerelease = 'rc'
+            prerelease += reg.group(2) if reg.group(2) else '1'
+        final = '{0}{1}'.format(version, prerelease if prerelease else '')
+        with open(verfile, 'w') as f:
+            return f.write(final)
+        return final
+    else:
+        raise ValueError("VERSION could not be parsed")
+
+
+def use_freenas(sdist, keyword, value):
+
+    assert keyword == 'use_freenas'
+
+    sdist.version = get_version()
