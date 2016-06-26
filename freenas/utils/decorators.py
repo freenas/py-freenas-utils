cc68ceed041ca52be4aa05b2ee1a74a8ae40889b
Wojciech Kloska <shangteus@ixsystems.com>
2016-06-26 19:14:52 +0200

    ADDED: function throttle decorator

--- freenas/utils/decorators.py
+++ freenas/utils/decorators.py
@@ -25,6 +25,7 @@
 #
 #####################################################################
 import time
+from datetime import datetime, timedelta
 from functools import wraps
 
 
@@ -42,6 +43,26 @@ class delay(object):
                 self.called = True
                 time.sleep(self.delay_time)
                 self.called = False
+                return fn(*args, **kwargs)
+
+        return wrapper
+
+
+class throttle(object):
+    def __init__(self, seconds=0, minutes=0, hours=0):
+        self.throttle_period = timedelta(
+            seconds=seconds, minutes=minutes, hours=hours
+        )
+        self.time_of_last_call = datetime.min
+
+    def __call__(self, fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            now = datetime.now()
+            time_since_last_call = now - self.time_of_last_call
+
+            if time_since_last_call > self.throttle_period:
+                self.time_of_last_call = now
                 return fn(*args, **kwargs)
 
         return wrapper
